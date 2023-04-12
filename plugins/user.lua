a5e9ea463523289e909ec12d3f2a3ec1e611e2b4
Samuel Mendenhall <smendenh@cisco.com>
2023-04-12 11:15:11 -0400

    Adding plugins and config

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,28 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    'Equilibris/nx.nvim',
+    requires = {
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require("nx").setup {}
+    end
+  },
+  { "phaazon/hop.nvim",
+    event = "BufRead",
+    config = function()
+      require("hop").setup()
+      vim.api.nvim_set_keymap("n", "s", ":HopChar2<cr>", { silent = true })
+      vim.api.nvim_set_keymap("n", "S", ":HopWord<cr>", { silent = true })
+    end,
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "BufRead",
+    config = function()
+      require("todo-comments").setup()
+    end,
+  },
 }
